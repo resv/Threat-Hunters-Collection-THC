b8915950b62f9b728213cfb44f0bcbe53aaa6b69
Adam Kim <akim456@gmail.com>
2024-02-10 12:52:09 -0500

    Added more tools
    Added menu, possible tools. Renamed variables to scale better for growth.
    
    Will implement sysmon execution next

--- DBCLI2go.ps1
+++ DBCLI2go.ps1
@@ -3,7 +3,39 @@
 
 
 # Initialize Variables (go down to execution line)
-$Banner = @"
+$BannerA = @"
+
+_____________________________________________________________________________
+
+                __  ______  ___________   _____   ____________ 
+               / / / / __ \/ ___/_  __/  /  _/ | / / ____/ __ \
+              / /_/ / / / /\__ \ / /     / //  |/ / /_  / / / /
+             / __  / /_/ /___/ // /    _/ // /|  / __/ / /_/ / 
+            /_/ /_/\____//____//_/    /___/_/ |_/_/    \____/                                                     
+                                                                                            
+                                                     Powered by Cyber Seidon
+_____________________________________________________________________________									 
+
+"@
+
+
+
+$BannerB = @"
+
+_____________________________________________________________________________
+
+                   _______  _______ __  _______  _   __
+                  / ___/\ \/ / ___//  |/  / __ \/ | / /
+                  \__ \  \  /\__ \/ /|_/ / / / /  |/ / 
+                 ___/ /  / /___/ / /  / / /_/ / /|  /  
+                /____/  /_//____/_/  /_/\____/_/ |_/                                           
+                                                                                                                                    
+                                                     Powered by Cyber Seidon
+_____________________________________________________________________________									 
+
+"@
+
+$BannerC = @"
 
 _____________________________________________________________________________
 
@@ -18,13 +50,32 @@ _____________________________________________________________________________
 
 "@
 
+$BannerD = @"
+
+_____________________________________________________________________________
+
+                ___   __  ____________  ____  __  ___   _______
+               /   | / / / /_  __/ __ \/ __ \/ / / / | / / ___/
+              / /| |/ / / / / / / / / / /_/ / / / /  |/ /\__ \ 
+             / ___ / /_/ / / / / /_/ / _, _/ /_/ / /|  /___/ / 
+            /_/  |_\____/ /_/  \____/_/ |_|\____/_/ |_//____/  
+                                                                                    
+                                                     Powered by Cyber Seidon
+_____________________________________________________________________________									 
+
+"@
+
+
 $HealthCheck = "False"
 $Hostname = hostname
 $Profile = $env:userprofile
 $PrintWorkingDirectory = pwd
 $AppURL = "https://github.com/sans-blue-team/DeepBlueCLI.git"
-$ParentAppFolder = "DBCLI"
-$AppName = "DeepBlueCLI"
+$ParentAppCFolder = "DBCLI"
+$SysmonFolder = "Sysmon vXX.XX"
+$AppBName = "Sysmon vXX.XX"
+$AppCName = "DeepBlueCLI"
+$AppDName = "Sysinternals"
 $IPAddress = $env:HostIP = (
     Get-NetIPConfiguration |
     Where-Object {
@@ -33,20 +84,60 @@ $IPAddress = $env:HostIP = (
     }
 ).IPv4Address.IPAddress
 
+$Menu = @" 
+$BannerC
+Waiting for Host information
+[A] Get Host information
+[B] Download & Install $AppBName 
+[C] Download & Run $AppCName
+[D] Download & Run $AppDName
+"@
 
-$StatusCreatedParentAppFolder = @"
----------- [ Created folder on your desktop called `"$ParentAppFolder`" ] ---------- `n
+
+
+$StatusCreatedParentAppCFolder = @"
+---------- [ Created folder on your desktop called `"$ParentAppCFolder`" ] ---------- `n
 "@
 $StatusDownloadedApp = @"
----------- [ Downloaded and extracted `"$AppName`" ] ----------------- `n
+---------- [ Downloaded and extracted `"$AppCName`" ] ----------------- `n
 "@
 $StatusChangedDirToAppFolder = @"
----------- [ Changed working directory to `"$AppName`" ] ------------- `n
+---------- [ Changed working directory to `"$AppCName`" ] ------------- `n
 "@
 
 
 # Execution starts here:
 # -------------------------------------------------------------------
+while($true) {
+    $readHostValue = Read-Host -Prompt "$Menu"
+    switch ($readHostValue) {
+        'a' {
+            #Insert logic here
+            return #Exits the script
+        }
+        'b' {
+            #Insert logic here
+            return #Exits the script
+        }
+        'c' {
+            #Insert logic here
+            return #Exits the script
+        }
+        'd' {
+            #Insert logic here
+            return #Exits the script
+        }
+
+        Default {
+            Write-Host "Invalid Input"
+        }
+    }
+}
+
+
+
+
+
 # Change CWD to the desktop
 set-location "$($env:userprofile)\Desktop\"
 
@@ -59,7 +150,7 @@ Write-Host $Banner
 Write-host "[Application URL]:" $AppURL
 
 # Notify working directory
-Write-Host "[PWD]:" $PrintWorkingDirectory\$ParentAppFolder
+Write-Host "[PWD]:" $PrintWorkingDirectory\$ParentAppCFolder
 
 # Notify hostname & IP address
 Write-Host "[Hostname]:" $Hostname
@@ -68,22 +159,22 @@ Write-Host "[IP Address ]:" $IPAddress
 
 "`n"
 
-# Create the ParentAppFolder (Also hiding the Powershell Output)
-$null = new-item -path "$($env:userprofile)\Desktop" -name $ParentAppFolder -itemtype directory -Force
-Write-Host $StatusCreatedParentAppFolder
+# Create the ParentAppCFolder (Also hiding the Powershell Output)
+$null = new-item -path "$($env:userprofile)\Desktop" -name $ParentAppCFolder -itemtype directory -Force
+Write-Host $StatusCreatedParentAppCFolder
 
-# Change the directory to ParentAppFolder
-set-location "$($env:userprofile)\Desktop\$ParentAppFolder"
+# Change the directory to ParentAppCFolder
+set-location "$($env:userprofile)\Desktop\$ParentAppCFolder"
 
 # Download zip file from Repo, extract zip, rename zip, delete downloaded zip file
-Invoke-WebRequest 'https://github.com/sans-blue-team/DeepBlueCLI/archive/refs/heads/master.zip' -OutFile .\$AppName.zip
-Expand-Archive .\$AppName.zip .\
-Rename-Item .\$AppName-master .\$AppName
-Remove-Item .\$AppName.zip
+Invoke-WebRequest 'https://github.com/sans-blue-team/DeepBlueCLI/archive/refs/heads/master.zip' -OutFile .\$AppCName.zip
+Expand-Archive .\$AppCName.zip .\
+Rename-Item .\$AppCName-master .\$AppCName
+Remove-Item .\$AppCName.zip
 Write-Host $StatusDownloadedApp
 
-# Change the directory to AppName
-set-location "$($env:userprofile)\Desktop\$ParentAppFolder\$AppName"
+# Change the directory to AppCName
+set-location "$($env:userprofile)\Desktop\$ParentAppCFolder\$AppCName"
 Write-Host $StatusChangedDirToAppFolder
 
 # Check if staging and initialization is complete
